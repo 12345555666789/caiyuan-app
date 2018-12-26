7a4e8be25cd4631c634ebfddb2dc5ef04246c443
zexinPC <502071587@qq.com>
2018-12-26 21:07:58 +0800

    修复bug

--- src/config/ip.config.js
+++ src/config/ip.config.js
@@ -1,8 +1,8 @@
 const IP = {
   // dev: 'http://39.104.26.183:9080/api',
-  dev: 'http://221.208.141.188:9080/api',
+  dev: 'https://221.208.141.188/api',
   // production: 'http://39.104.26.183:9080/api'
-  production: 'http://221.208.141.188:9080/api'
+  production: 'https://221.208.141.188/api'
 };
 
 export default IP
